0378bc1aff1446d1b24e3c8eb2557fa929849c78
inmove <lisper.inmove@gmail.com>
2025-07-09 22:38:18 +0800

    smart_ptr demo

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,19 @@
 #include <iostream>
-#include <memory>
-#include <ostream>
-#include <utility>
-
-void smart_pointer() {
-    // 1. 一个对象只能被一个unique_ptr管理，不能复制，只能移动
-    std::unique_ptr<int> p1 = std::make_unique<int>(10);
-    // std::unique_ptr<int> p2 = p1;
-    std::unique_ptr<int> p2 = std::move(p1);
-    std::cout << p1 << std::endl;
-
-    // 2. 多个 shared_ptr 可以共同管理同一个对象，内部维护引用计数。
-    std::shared_ptr<int> p3 = std::make_shared<int>(11);
-    std::shared_ptr<int> p4 = p3;  // 引用计数+1
-    std::cout << *p3 << "," << *p4 << std::endl;
-    *p3 = 13;
-    std::cout << *p3 << "," << *p4 << std::endl;
-
-    // 3. 不拥有资源，仅做观察者，不影响引用计数；避免循环引用。
-    std::shared_ptr<int> p5 = std::make_shared<int>(12);
-    std::weak_ptr<int> p6   = p5;  // 没有引用计数
-    if (auto tmp = p6.lock()) {
-        *tmp = 13;
-        std::cout << ">>" << *tmp << ">>" << std::endl;
-    } else {
-        std::cout << "Expired" << std::endl;
-    }
-    std::cout << ">>" << *p5 << ">>" << std::endl;
-}
+#include "my_smart_ptr.h"
 
 int main() {
-    smart_pointer();
+    shared_ptr<int> ptr1(new int(10));
+    shared_ptr<int> ptr2 = ptr1;
+    std::cout << "ptr1: " << *ptr1 << "," << *ptr2 << std::endl;
+    ptr1.reset();
+    // Error
+    // std::cout << "ptr1: " << *ptr1 << std::endl;
+    std::cout << "ptr: " << *ptr2 << std::endl;
+
+    shared_ptr<int> ptr3 = std::move(ptr2);
+    std::cout << "ptr: " << *ptr3 << std::endl;
+    // Error
+    // std::cout << "ptr: " << *ptr2 << std::endl;
+
     return 0;
 }
